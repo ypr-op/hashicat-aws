da49990002b2e52a74aa385df92f91156ce16f68
YPR <yp.rumeur@groupeonepoint.com>
2024-02-28 15:58:50 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  bucket_prefix = "yprumeur" 
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/yprumeur/s3-bucket/aws"
+  version = "2.8.0"
 }
